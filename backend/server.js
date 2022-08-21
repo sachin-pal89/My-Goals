c413a5e41dde7a398071dd60dca07901a95b9f3e
Sachin Pal <sachinpal01012003@gmail.com>
2022-08-21 11:44:12 +0530

    Created Goals section for Server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,19 @@
-console.log("Hello World");
+import express from "express";
+import dotenv from "dotenv/config";
+import errorHandler from "../middleware/errorMiddleware.js";
+import routes from "./routes/goalRoutes.js";
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.use("/api/goals", routes)
+
+app.use(errorHandler)
+
+app.listen(port, () => {
+    console.log(`Hello Sachin ${port}`);
+})
